f04d82b8c11e72e5cb5c25a085a991f7a1514736
Ivan Reis <109477902+Ivan-ReisDev@users.noreply.github.com>
2023-03-18 11:27:36 -0300

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,7 @@ var $p = document.querySelector('.senha');
 var $senha = document.querySelector('.senha');
 var $numeros = document.querySelector('#numeros');
 var $especial = document.querySelector('#especial');
+var $update = document.querySelector('.update');
 var letras = ['a', 'q', 'w', 'r', 't', 'y', 'y', 'i', 'o', 'p', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'v', 'b', 'c', 'm', 'A', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B','N','M']
 var numeros = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 var especial = ['!', '@', '#', '$', '%', '&', '(', ')', '-', '_']
@@ -60,6 +61,18 @@ function GerarSenhaEspecial() {
 }
 
 $tamanhoSenha.addEventListener('change', () => {
+    if ($numeros.checked == true & $especial.checked == true) {
+        GerarSenhaNumeroEspecial()
+    } else if ($especial.checked == true & $numeros.checked == false) {
+        GerarSenhaEspecial()
+    } else if ($numeros.checked == true & $especial.checked == false) {
+        GeraSenhaNumero()
+    } else {
+        GerarsenhaLetra()
+    }
+})
+
+$update.addEventListener('click',() => {
     if ($numeros.checked == true & $especial.checked == true) {
         GerarSenhaNumeroEspecial()
     } else if ($especial.checked == true & $numeros.checked == false) {
